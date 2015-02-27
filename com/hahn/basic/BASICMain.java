5c9048f9484305b58c1e43da39f1e660048bffad
John Espenhahn <john@espenhahn.org>
2015-02-27 11:42:56 -0500

    Update version code

--- com/hahn/basic/BASICMain.java
+++ com/hahn/basic/BASICMain.java
@@ -18,7 +18,7 @@ import com.hahn.basic.util.exceptions.CompileException;
 import com.hahn.basic.viewer.ViewerBuilder;
 
 public class BASICMain extends Main {
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "2.0.0";
     
     private final ILexer lexer;
     private final Parser parser;
